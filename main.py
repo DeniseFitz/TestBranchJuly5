79603ec52fbe2401e95bc56f7400e59aec65ccd1
Denise Poll <denise.poll@seo-usa.org>
2022-07-06 15:05:03 +0000

    updated tests for coverage

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import requests
+import json
+import urllib
+from sqlalchemy import create_engine
 
-def carData():
-    # https://rapidapi.com/principalapis/api/car-data/
-    return
+def createDatabase():
+    #create database cars; 
+    return 
 
+def getData():
+    where = urllib.parse.quote_plus("""
+    {
+        "Year": {
+        "$exists": true
+        }
+    }
+    """)
+
+    url = 'https://parseapi.back4app.com/classes/Carmodels_Car_Model_List_Ferrari?count=1&limit=0&where=%s' % where
+    headers = {
+    'X-Parse-Application-Id': 'RcA7xAHt05iSSYfQAq5O6IP9s16Askxnc07XJ7t1', # This is your app's application id
+    'X-Parse-REST-API-Key': '1rUQAkAwRg5133HdwtCPbw0IiaHcpVji74Z38ZQf' # This is your app's REST API key
+    }
+    data = json.loads(requests.get(url, headers=headers).content.decode('utf-8')) # Here you have the data that you need
+    print(json.dumps(data, indent=2))
+
+def createTable():
+    '''CREATE TABLE mytable ( 
+	    id INT(8) UNSIGNED NOT NULL auto_increment,
+	    model VARCHAR(255) default NULL,
+	    year YEAR(4) default NULL,
+	    brand VARCHAR(255) default NULL,
+	    PRIMARY KEY (id)
+	) AUTO_INCREMENT=1;'''
 
 print('main.py here')
+getData()
