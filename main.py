3b324e2dad4818691fbcca706ffe1836899e49d5
Denise Poll <denise.poll@seo-usa.org>
2022-07-07 15:00:03 +0000

    updates code style

--- main.py
+++ main.py
@@ -6,43 +6,41 @@ from sqlalchemy import create_engine
 
 engine = create_engine('sqlite://', echo=False)
 
+
 def add_fun(num1, num2):
     return (num1 + num2)
 
 
 def createDatabase():
-    #create database cars; 
-    return 
+    return
+
 
 def getData():
-    url = 'https://vpic.nhtsa.dot.gov/api//vehicles/GetMakesForManufacturerAndYear/mer?year=2013&format=json';
-    r = requests.get(url);
-    print(type(r.text));
+    url = 'https://vpic.nhtsa.dot.gov/api//vehicles/'
+    url2 = 'GetMakesForManufacturerAndYear/mer?year=2013&format=json'
+    r = requests.get(url+url2)
+    print(type(r.text))
 
     df = pd.read_json(r.text)
     print("from json to df ", df.head())
-    #need to get json Results column to a df
-    #https://www.geeksforgeeks.org/python-pandas-flatten-nested-json/
+    # need to get json Results column to a df
+    # https://www.geeksforgeeks.org/python-pandas-flatten-nested-json/
     normaldf = pd.json_normalize(df["Results"])
     print("results normalized ", normaldf.head())
 
-    #df to sql
+    # df to sql
     normaldf.to_sql('newDB', con=engine)
     print("new database type ", type('newDB'))
 
-    #query result goes to a dataframe
-    alphabetized = engine.execute("SELECT MakeName FROM newDB ORDER BY MakeName ASC limit 10;")  
+    # query result goes to a dataframe
+    alphabetized = engine.execute(
+        "SELECT MakeName FROM newDB ORDER BY MakeName ASC limit 10;")
     print("alpha names from query ", pd.DataFrame(alphabetized))
 
 
 def createTable():
-    '''CREATE TABLE mytable ( 
-	    id INT(8) UNSIGNED NOT NULL auto_increment,
-	    model VARCHAR(255) default NULL,
-	    year YEAR(4) default NULL,
-	    brand VARCHAR(255) default NULL,
-	    PRIMARY KEY (id)
-	) AUTO_INCREMENT=1;'''
+    return
+
 
 print('main.py here')
 getData()
--- main_test.py
+++ main_test.py
@@ -4,7 +4,8 @@ from main import add_fun
 
 class TestFileName(unittest.TestCase):
     def test_add_fun(self):
-        self.assertEqual(add_fun(1,5), 6)
+        self.assertEqual(add_fun(1, 5), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
